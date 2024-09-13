1c4411666cd6b15b3c5aabf215aa419f83b5ec25
Stefan Mileusnić <st.mileusnic@gmail.com>
2024-09-13 19:47:50 +0200

    Add files via upload

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://129f9c614a47fa6b6de3725f3235b7f1@o4507817710649344.ingest.de.sentry.io/4507817713533008",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
